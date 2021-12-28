a2029252b6c736f2af411068783ac2e07013708b
emilrichardo <emilrichardo@gmail.com>
2021-12-28 18:06:05 -0300

    enlaces botones

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,15 @@ import SkillBar from "../components/SkillBar";
 export default function Home() {
   return(
   <>
-  <div className="text-center  h-screen   flex  items-center justify-center">
+
+
+  <div className="text-center  h-screen   flex  items-center justify-center z-10 relative top-0" >
+
     <div className="py-5 ">
+
      <StaticImage
         src="../assets/emil.jpg"
-        className="rounded-full h-24 w-24 md:h-44 md:w-44 mb-8 border-2 border-primary"
+        className=" md:hidden rounded-full h-24 w-24 md:h-44 md:w-44 mb-8 border-2 border-primary"
         alt="image profile"
         width={200}
         height={200}
@@ -42,16 +46,18 @@ export default function Home() {
         <SiAdobephotoshop/>
         <SiDocker/>
       </div>
-      <Button className="block mx-2 mb-2" variant="primary mb-8" to="https://google.com"><AiFillBehanceCircle className="mr-2 -ml-2"/>Behance</Button>
-      <Button className="block mx-2 mb-2" variant="primary"  to="https://google.com"><AiFillLinkedin className="mr-2 -ml-2"/>Linkedin</Button>
+      <Button className="block mx-2 mb-2" variant="primary mb-8" to="https://www.behance.net/emilgonzalez"><AiFillBehanceCircle className="mr-2 -ml-2"/>Behance</Button>
+      <Button className="block mx-2 mb-2" variant="primary"  to="https://www.linkedin.com/in/emilgonzalez/"><AiFillLinkedin className="mr-2 -ml-2"/>Linkedin</Button>
 
-      <footer className="py-24">
+      <footer className="py-24 flex flex-col">
       <a href="mailto:emilrichardo@gmail.com">emilrichardo@gmail.com</a>
+      <a href="tel:+543855963936">+54 3855963936</a>
     </footer>
     </div>
 
 
   </div>
+
 
   </>)
 }
