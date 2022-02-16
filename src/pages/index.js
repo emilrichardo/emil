39fc1eae370ec68a7a4ba486c6ec07c22256a32c
emilrichardo <emilrichardo@gmail.com>
2022-02-16 16:50:31 -0300

    flex md container

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   <>
 
 
-  <div className="text-center  h-screen   flex  items-center justify-center z-10 relative top-0" >
+  <div className="text-center  h-screen   md:flex  items-center justify-center z-10 relative top-0" >
     <div className="py-5 ">
     <div>
         <StaticImage
@@ -26,14 +26,14 @@ export default function Home() {
       </div>
 
       <h1 className="text-4xl md:text-8xl  font-medium  mb-4">Emil Gonzalez</h1>
-      <h2 className="text-xl   mb-12">UX/UI Designer - Frontend developer</h2>
-      <div className="flex flex-col md:flex-row">
+      <h2 className="text-xl px-4  mb-12">UX/UI Designer - Frontend developer</h2>
+      <div className="px-4 flex flex-col md:flex-row">
         <Button className="block mx-2 mb-2" variant="default"  to="https://www.linkedin.com/in/emilgonzalez/"><AiFillLinkedin className="mr-2 -ml-2"/>Linkedin</Button>
         <Button className="block mx-2 mb-2" variant="default" to="https://www.behance.net/emilgonzalez"><AiFillBehanceCircle className="mr-2 -ml-2"/>Behance</Button>
         <Button className="block mx-2 mb-2" variant="default"  to="https://github.com/emilrichardo"><SiGithub className="mr-2 -ml-2"/>Github</Button>
       </div>
 
-      <footer className="py-24 flex flex-col">
+      <footer className="py-14 flex flex-col">
       <a className=" underline" href="mailto:emilrichardo@gmail.com">emilrichardo@gmail.com</a>
       <a href="tel:+543855963936">+54 3855963936</a>
 
