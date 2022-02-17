04f53928b5ca6f89141a8e8b605a189346f795eb
emilrichardo <emilrichardo@gmail.com>
2022-02-17 18:25:45 -0300

    logo link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { StaticImage } from "gatsby-plugin-image"
+import { Link } from 'gatsby';
 export default function Header() {
     return(
         <nav className="flex justify-between py-4 px-4">
-            <StaticImage
-            src="../assets/favicon.svg"
-            className=" h-12 w-12"
-            alt="logo emil"
-            />
+            <Link
+            to='/'
+            >
+                <StaticImage
+                src="../assets/favicon.svg"
+                className=" h-12 w-12"
+                alt="logo emil"
+                />
+            </Link>
+
             <span className="flex md:items-center flex-col md:flex-row   text-right">
             <a className="underline md:mr-4" href="mailto:emilrichardo@gmail.com">emilrichardo@gmail.com</a>
             <a href="tel:+543855963936">+54 3855963936</a>
